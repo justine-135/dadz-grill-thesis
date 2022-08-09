3e30b926c3184a2124dab61678b646c249b9c55d
Justine <ujusine@yahoo.com>
2022-08-09 21:18:47 +0800

    add class selector to modal head text

--- foods.php
+++ foods.php
@@ -5,7 +5,7 @@
 
 <div class="overlay">
     <form class="form-overlay insert-item-form" action="includes/food-contr.inc.php" id="insert-ing" method="post" enctype="multipart/form-data">
-        <div class="head flex-row">
+        <div class="head flex-row modal-head">
             <h4>Add Item</h4>
             <button class="close-add-form" type="button"><svg class="sidelink-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg></button>
         </div>
@@ -46,7 +46,7 @@
     </form>
 
     <form class="form-overlay update-item-form" action="includes/food-contr.inc.php" method="post" enctype="multipart/form-data">
-    <div class="head flex-row">
+    <div class="head flex-row modal-head">
             <h4>Update Item</h4>
             <button class="close-add-form" type="button"><svg class="sidelink-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg></button>
         </div>
@@ -85,7 +85,7 @@
 
     
     <form class="form-overlay delete-item-form" action="includes/food-contr.inc.php" method="post" enctype="multipart/form-data">
-        <div class="head flex-row">
+        <div class="head flex-row modal-head">
             <h4>Delete</h4>
             <button class="close-add-form" type="button"><svg class="sidelink-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg></button>
         </div>
@@ -112,14 +112,13 @@
 
 <div class="main-content ingridient-container">
     <div class="ingridients">
-    <table class="inventory-table ing-tbl">
+    <table class="tables-table">
         <?php include "includes/foods-view.inc.php"; ?>
     </table>
     </div>
     
 </div>
 
-<script src="js/foods.js"></script>
 <script src="js/inventory.js"></script>
 
 <?php }
