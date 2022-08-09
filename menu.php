e9439d6b4ad062ee70453d65dc1ef57ddf4ce90f
Justine <ujusine@yahoo.com>
2022-08-09 21:19:10 +0800

    add class selector to modal head text and changed table class name

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
 
 <div class="overlay">
     <form class="action-btn-modal" action="./includes/table-contr.inc.php" method="post" enctype="multipart/form-data">
-        <div class="head flex-row">
+        <div class="head flex-row modal-head">
             <h4 class="action-modal-head"></h4>
             <button type="button"><svg class="sidelink-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg></button>
         </div>
@@ -28,7 +28,7 @@
 </nav>
 <div class="main-content tables-container waiter-tbl">
     <div class="tables waiter-tbl">
-        <table class="waiter-table waiter-tbl-data">
+        <table class="tables-table waiter-table waiter-tbl-data">
         </table>
     </div>
 </div>
