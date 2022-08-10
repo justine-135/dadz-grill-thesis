d2a67d9254874d452063f96a6ab2d6d66f117a81
Justine <ujusine@yahoo.com>
2022-08-10 17:49:20 +0800

    get each table statuses total

--- classes/dashboardview.class.php
+++ classes/dashboardview.class.php
@@ -13,7 +13,7 @@ class DashboardView extends Dashboard{
     }
 
     public function initSales(){
-        $results = $this->getSales();
+        $results = $this->getTransactions();
 
         $total = 0;
         $total = (int)$total;
@@ -21,6 +21,92 @@ class DashboardView extends Dashboard{
             if ($row["paid"] == 1) {
                 $val = (int)$row["total"];
                 $total += (int)$val;
+            }
+        }
+
+        echo $total;
+    }
+
+    public function initCancels(){
+        $results = $this->getTransactions();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["paid"] == 3) {
+                $val = (int)$row["paid"];
+                $total += (int)$val;
+            }
+        }
+
+        echo $total;
+    }
+
+    public function initSuccess(){
+        $results = $this->getTransactions();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["paid"] == 1) {
+                $val = (int)$row["paid"];
+                $total += (int)$val;
+            }
+        }
+
+        echo $total;
+    }
+
+    public function initOccupied(){
+        $results = $this->getTables();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["table_status"] == "Occupied") {
+                $total += 1;
+            }
+        }
+
+        echo $total;
+    }
+
+    public function initUnoccupied(){
+        $results = $this->getTables();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["table_status"] == "Unoccupied") {
+                $total += 1;
+            }
+        }
+
+        echo $total;
+    }
+
+    public function initCall(){
+        $results = $this->getTables();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["table_status"] == "Call") {
+                $total += 1;
+            }
+        }
+
+        echo $total;
+    }
+
+    public function initDirty(){
+        $results = $this->getTables();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["table_status"] == "Dirty") {
+                $total += 1;
             }
         }
 
