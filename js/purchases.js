988d69ac9f273bcba306d6417252cf3f94e98137
Justine <ujusine@yahoo.com>
2022-08-10 17:09:39 +0800

    add modal for cancel option

--- js/purchases.js
+++ js/purchases.js
@@ -3,14 +3,24 @@ window.addEventListener("load", () => {
   dashboardLi.classList.add("active");
   dashboardLi.querySelector(".inactive-link").className = "active-link";
 
-  const cancelBtn = document.querySelectorAll(".cancel-btn");
   const overlay = document.querySelector(".overlay");
-  const closeBtn = document.querySelector(".close-add-form");
+  const closeBtn = document.querySelectorAll(".close-add-form");
   const orderNumber = document.querySelector(".order-number");
-  const orderId = document.querySelector(".table-order");
+  const orderNumber2 = document.querySelector(".order-number2");
 
-  closeBtn.addEventListener("click", () => {
-    overlay.classList.remove("open");
+  const orderId = document.querySelector(".order-num");
+  const orderId2 = document.querySelector(".order-num2");
+
+  const tableId = document.querySelector(".table-order");
+  const tableId2 = document.querySelector(".table-order2");
+
+  const finishDiv = document.querySelector(".confirm-finish-cancel");
+  const cancelDiv = document.querySelector(".confirm-finish-cancel2");
+
+  closeBtn.forEach(element => {
+    element.addEventListener("click", () => {
+      overlay.classList.remove("open");
+    });
   });
 
   const confirmOrder = (id) => {
@@ -25,22 +35,57 @@ window.addEventListener("load", () => {
     xmlhttp.send();
   };
 
+  const cancelOrder = (id) => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        document.querySelector(".order-confirm-cancel").innerHTML =
+          this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "./includes/purchase-view.inc.php?id=" + id, true);
+    xmlhttp.send();
+  };
+
   const loadOrders = () => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         document.querySelector(".orders-list").innerHTML = this.responseText;
+
         const finishBtn = document.querySelectorAll(".finish-btn");
+        const cancelBtn = document.querySelectorAll(".cancel-btn");
+
         finishBtn.forEach((element) => {
           element.addEventListener("click", () => {
+            finishDiv.classList.add("open");
+            cancelDiv.classList.remove("open");
             overlay.classList.add("open");
             let id =
               element.parentElement.parentElement.childNodes[1].innerHTML;
             orderNumber.innerHTML = "Finish order - " + id;
             orderId.value =
-              element.parentElement.parentElement.childNodes[3].innerHTML;
+              element.parentElement.parentElement.childNodes[1].innerHTML;
+            tableId.value = element.parentElement.parentElement.childNodes[3].innerHTML;
 
+            console.log(element.parentElement.parentElement.childNodes[3].innerHTML);
             confirmOrder(id);
+          });
+        });
+
+        cancelBtn.forEach((element) => {
+          element.addEventListener("click", () => {
+            overlay.classList.add("open");
+            cancelDiv.classList.add("open");
+            finishDiv.classList.remove("open");
+            let id =
+              element.parentElement.parentElement.childNodes[1].innerHTML;
+            orderNumber2.innerHTML = "Cancel order - " + id;
+            orderId2.value =
+              element.parentElement.parentElement.childNodes[1].innerHTML;
+              tableId2.value = element.parentElement.parentElement.childNodes[3].innerHTML;
+
+            cancelOrder(id);
           });
         });
       }
