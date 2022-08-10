cfabdb09ee6a57e0fd4c2cd30fcc972c18089d68
Justine <ujusine@yahoo.com>
2022-08-10 17:49:41 +0800

    create object for each table status

--- includes/dashboard-view.inc.php
+++ includes/dashboard-view.inc.php
@@ -8,14 +8,40 @@
             $order = new DashboardView();
             $order->initSales();
         }
-    }
-
-    if (isset($_GET["grid"])) {
-        $grid = $_GET["grid"];
 
         if ($grid == 2) {
             $order = new DashboardView();
             $order->initOrders();
+        }
+
+        if ($grid == 3) {
+            $order = new DashboardView();
+            $order->initCancels();
+        }
+
+        if ($grid == 4) {
+            $order = new DashboardView();
+            $order->initSuccess();
+        }
+
+        if ($grid == 5) {
+            $occupied = new DashboardView();
+            $occupied->initOccupied();
+        }
+
+        if ($grid == 6) {
+            $unoccupied = new DashboardView();
+            $unoccupied->initUnoccupied();
+        }
+
+        if ($grid == 7) {
+            $call = new DashboardView();
+            $call->initCall();
+        }
+
+        if ($grid == 8) {
+            $dirty = new DashboardView();
+            $dirty->initDirty();
         }
     }
 ?>
