10495e7007d142700174bc38197a1c4aeb8187b9
Justine <ujusine@yahoo.com>
2022-08-10 17:04:50 +0800

    show orders, and sales count

--- classes/dashboardview.class.php
+++ classes/dashboardview.class.php
@@ -3,14 +3,28 @@
 class DashboardView extends Dashboard{
     public function initOrders(){
         $results = $this->getOrders();
+
+        echo $results;
     }
 
     public function initCrews(){
         $results = $this->getCrews();
+
     }
 
     public function initSales(){
         $results = $this->getSales();
+
+        $total = 0;
+        $total = (int)$total;
+        foreach ($results as $row) {
+            if ($row["paid"] == 1) {
+                $val = (int)$row["total"];
+                $total += (int)$val;
+            }
+        }
+
+        echo $total;
     }
 }
 
