c0f7af2cc0ba0556d021e562deebbd9f14939bfb
Justine <ujusine@yahoo.com>
2022-08-10 17:04:26 +0800

    queries sales, orders

--- classes/dashboard.class.php
+++ classes/dashboard.class.php
@@ -1,12 +1,21 @@
 <?php
 
 class Dashboard extends Dbh{
-    protected function getOrders(){
-        $sql = "SELECT * FROM inventory;";
+    protected function getSales(){
+        $sql = "SELECT * FROM transactions;";
         $stmt = $this->connection()->prepare($sql);
         $stmt->execute();
 
         $results = $stmt->fetchAll();
+        return $results;
+    }
+
+    protected function getOrders(){
+        $sql = "SELECT * FROM transactions;";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute();
+
+        $results = $stmt->rowCount();
         return $results;
     }
 }
