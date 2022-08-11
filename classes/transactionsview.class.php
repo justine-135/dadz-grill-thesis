f5d1ccc1b5cbb9c804277c1b160538c0dfeff518
Justine <ujusine@yahoo.com>
2022-08-11 19:04:08 +0800

    updated view

--- classes/transactionsview.class.php
+++ classes/transactionsview.class.php
@@ -9,7 +9,7 @@ class TransactionsView extends Transactions{
                 <th>Order no.</th>
                 <th>Table no.</th>
                 <th>Time</th>
-                <th>Order</th>
+                <th>Orders</th>
                 <th>Total</th>
                 <th>Status</th>
                 <th class="status-action-col">Status Action</th>
@@ -24,12 +24,47 @@ class TransactionsView extends Transactions{
                 <td class="pad10" valign="top"><?php echo $row['table_id'] ?></td>
                 <td class="pad10" valign="top"><?php echo $row['reg_date'] ?></td>
                 <td>
+                    <?php 
+                        $result = explode("|",$row['order']);
+                        $result2 = explode("|",$row['quantity']);
+                    ?>
                     <table class="orders-table">
                         <tbody>
-                            <?php echo $row['order'] ?>
+                            <?php
+                                for ($i=0; $i < (count($result) - 1); $i++) { 
+                            ?>
+                                <tr>
+                                    <td>
+                                        <?php
+                                            echo $result[$i];
+                                        ?>
+                                    </td>
+                                    <td class="order-x-col">
+                                        x
+                                    </td>
+                                    <td>
+                                        <?php
+                                            echo $result2[$i];
+                                        ?>
+                                    </td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
                         </tbody>
-                    </table></td>
-                <td class="pad10" valign="top"><?php echo $row['total'] ?></td>
+                    </table>
+                </td>
+                <td class="pad10" valign="top">
+                <?php 
+                    $result = explode("|",$row['price']);
+                    $total = 0;
+                    for ($i=0; $i < (count($result) - 1); $i++) { 
+                        $total += $result[$i];
+                    }
+
+                    echo $total;
+                ?>    
+                </td>
                 <td class="pad10" valign="top"><?php if ($row['paid'] == 0) {
                     echo "Not paid";
                 }
