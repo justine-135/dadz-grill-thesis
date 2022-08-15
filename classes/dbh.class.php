d75a0f03214c4a30a349407c54d5eeff46126d49
Justine <ujusine@yahoo.com>
2022-08-16 00:07:24 +0800

    changed class name

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,24 +1,25 @@
 <?php
 
-class Dbh {
+class Dbh
+{
     private $host = "localhost";
     private $user = "root";
     private $pwd = "";
-    private $dbName = "inventory-system";
+    private $dbName = "dadz_grill_db";
 
-    protected function connection(){
-        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName; 
+    protected function connection()
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
         $conn = new PDO($dsn, $this->user, $this->pwd);
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $conn;
     }
 
-    protected function connectDb(){
-        $dsn = 'mysql:host=' . $this->host; 
+    protected function connectDb()
+    {
+        $dsn = 'mysql:host=' . $this->host;
         $conn = new PDO($dsn, $this->user, $this->pwd);
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $conn;
     }
-
-
 }
