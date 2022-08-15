a55bbfe6c797acdf6c55ef067afb389070b59c71
Justine <ujusine@yahoo.com>
2022-08-16 01:26:08 +0800

    remove unused container

--- store.php
+++ store.php
@@ -1,12 +1,13 @@
 <?php
-    session_start();
-    if (empty($_SESSION["username"])) {
-        header("location: login.php?message=");
-    }
+session_start();
+if (empty($_SESSION["username"])) {
+    header("location: login.php?message=");
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,16 +19,18 @@
     <link rel="stylesheet" href="./styles/styles.css">
     <title>Document</title>
 </head>
+
 <body>
     <nav class="store-nav flex-row">
         <div class="logo flex-row">
-            <svg class="store-back-icon icons" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" class="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg>
+            <svg class="store-back-icon icons" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" class="svg-inline--fa fa-chevron-right fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path>
+            </svg>
             <a href="menu.php">Back</a>
         </div>
     </nav>
     <aside class="cart-div ">
-        <form class="flex-column submit-sale" action="includes/purchase-contr.inc.php" method="post" enctype="multipart/form-data">
-            <nav class="cart-nav"></nav>
+        <form class="submit-sale flex-column" action="includes/purchase-contr.inc.php" method="post" enctype="multipart/form-data">
             <div class="cart-body flex-column">
                 <div class="cart-intro-text">
                     <h2>Table no. <?php echo $_GET['id'] ?></h2>
@@ -70,19 +73,19 @@
                     <div class="image"></div>
                     <div class="text">
                         <span>Sides</span>
-                    </div>               
-                 </button>
+                    </div>
+                </button>
                 <button id="drinks">
                     <div class="image"></div>
                     <div class="text">
                         <span>Drinks</span>
-                    </div>                
+                    </div>
                 </button>
                 <button id="addons">
                     <div class="image"></div>
                     <div class="text">
                         <span>Add-ons</span>
-                    </div>                
+                    </div>
                 </button>
             </div>
         </div>
@@ -91,19 +94,19 @@
                 <h1>All</h1>
                 <div class="group-btn item-btn">
                     <?php
-                        include "./includes/autoload.inc.php";
+                    include "./includes/autoload.inc.php";
 
-                        $allFoods = new FoodsView();
-                        $allFoods->initReadAll();
+                    $allFoods = new FoodsView();
+                    $allFoods->initReadAll();
                     ?>
                 </div>
             </div>
             <div class="sets-container">
                 <h1>Main sets</h1>
                 <div class="group-btn item-btn">
-                <?php
-                        $setFoods = new FoodsView();
-                        $setFoods->initReadSets();
+                    <?php
+                    $setFoods = new FoodsView();
+                    $setFoods->initReadSets();
                     ?>
                 </div>
             </div>
@@ -111,8 +114,8 @@
                 <h1>Sides</h1>
                 <div class="group-btn item-btn">
                     <?php
-                        $sideFoods = new FoodsView();
-                        $sideFoods->initReadSides();
+                    $sideFoods = new FoodsView();
+                    $sideFoods->initReadSides();
                     ?>
                 </div>
             </div>
@@ -120,17 +123,17 @@
                 <h1>Drinks</h1>
                 <div class="group-btn item-btn">
                     <?php
-                        $drinkFoods = new FoodsView();
-                        $drinkFoods->initReadDrinks();
+                    $drinkFoods = new FoodsView();
+                    $drinkFoods->initReadDrinks();
                     ?>
                 </div>
             </div>
             <div class="addons-container">
                 <h1>Addons</h1>
                 <div class="group-btn item-btn">
-                <?php
-                        $drinkFoods = new FoodsView();
-                        $drinkFoods->initReadAddons();
+                    <?php
+                    $drinkFoods = new FoodsView();
+                    $drinkFoods->initReadAddons();
                     ?>
                 </div>
             </div>
@@ -139,6 +142,7 @@
 
     <script src="./js/store.js"></script>
     <script src="./js/cart.js"></script>
-    
+
 </body>
+
 </html>
