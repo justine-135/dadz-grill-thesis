53571f2aeb0d0a333a60e7c88370a0d9afa199a1
Justine <ujusine@yahoo.com>
2022-08-16 19:05:01 +0800

    added url get message

--- classes/foodcontr.class.php
+++ classes/foodcontr.class.php
@@ -1,6 +1,7 @@
 <?php
 
-class FoodContr extends Foods{
+class FoodContr extends Foods
+{
     private $name;
     private $group;
     private $cost;
@@ -13,7 +14,8 @@ class FoodContr extends Foods{
     private $allowed;
     private $img;
 
-    public function __construct($name, $group, $cost, $stats, $fileActualExt, $fileTempLoc, $target, $fileError, $fileNameTime, $allowed, $fid, $img){
+    public function __construct($name, $group, $cost, $stats, $fileActualExt, $fileTempLoc, $target, $fileError, $fileNameTime, $allowed, $fid, $img)
+    {
         $this->name = $name;
         $this->group = $group;
         $this->cost = $cost;
@@ -28,17 +30,21 @@ class FoodContr extends Foods{
         $this->img = $img;
     }
 
-    public function initSetFood(){
+    public function initSetFood()
+    {
         $this->setFood($this->name, $this->group, $this->cost, $this->stats, $this->fileActualExt, $this->fileTempLoc, $this->target, $this->fileError, $this->fileNameTime, $this->allowed);
+        header("location: ../foods.php?alert=store&id=0");
     }
 
-    public function initUpdateFood(){
+    public function initUpdateFood()
+    {
         $this->updateFood($this->name, $this->group, $this->cost, $this->stats, $this->fid);
+        header("location: ../foods.php?alert=update&id=" . $this->fid);
     }
 
-    public function initDeleteFood(){
+    public function initDeleteFood()
+    {
         $this->deleteFood($this->img, $this->fid);
+        header("location: ../foods.php?alert=delete&id=" . $this->fid);
     }
 }
-
-?>
