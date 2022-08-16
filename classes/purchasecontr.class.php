5a6ce1c6a3cd539b4b837fbf031a6d5604cc109d
Justine <ujusine@yahoo.com>
2022-08-16 19:05:11 +0800

    added url get message

--- classes/purchasecontr.class.php
+++ classes/purchasecontr.class.php
@@ -1,20 +1,22 @@
 <?php
 
-class PurchaseContr extends Purchase{
-    public function initPurchase($names, $tableId, $total, $waiter, $qty, $prc, $uid){
+class PurchaseContr extends Purchase
+{
+    public function initPurchase($names, $tableId, $total, $waiter, $qty, $prc, $uid)
+    {
         $this->setPurchase($names, $tableId, $total, $waiter, $qty, $prc, $uid);
-        header("location: ../menu.php");
+        header("location: ../menu.php?alert=order_done&id=" . $tableId);
     }
 
-    public function initFinish($oid, $tid){
+    public function initFinish($oid, $tid)
+    {
         $this->setFinish($oid, $tid);
-        header("location: ../sales.php");
+        header("location: ../sales.php?alert=order_finish&id=" . $oid);
     }
 
-    public function initCancel($oid, $tid){
+    public function initCancel($oid, $tid)
+    {
         $this->setCancel($oid, $tid);
-        header("location: ../sales.php");
+        header("location: ../sales.php?alert=order_cancel&id=" . $oid);
     }
 }
-
-?>
