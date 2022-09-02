b096d17e650acd9c80b1674d3f82e7e6bb39a5aa
Justine <ujusine@yahoo.com>
2022-09-02 20:23:11 +0800

    insert data to admin model

--- classes/admincontr.class.php
+++ classes/admincontr.class.php
@@ -6,4 +6,11 @@ class AdminContr extends Admin
     {
         $this->deleteAdmin($id);
     }
+
+    public function initEdit($id, $fullname, $email, $contact, $bday, $address)
+    {
+        $this->editAdmin($id, $fullname, $email, $contact, $bday, $address);
+        header("location: ../profile.php?alert=updated&id=" . $id);
+
+    }
 }
