e5e673d47c94e3338a168233ddb3f3f39070ab50
Justine <ujusine@yahoo.com>
2022-09-02 20:25:44 +0800

    insert data to object for admincontr class

--- includes/admin-contr.inc.php
+++ includes/admin-contr.inc.php
@@ -4,7 +4,18 @@
     $id = $_POST["id-value"];
 
     if (isset($_POST["delete"])) {
-        $deleteTable = new AdminContr();
-        $deleteTable->initDelete($id);
+        $deleteAdmin = new AdminContr();
+        $deleteAdmin->initDelete($id);
+    }
+
+    if (isset($_POST["edit"])) {
+        $fullname = $_POST["fullname"];
+        $email = $_POST["email"];
+        $contact = $_POST["contact"];
+        $bday = $_POST["bdate"];
+        $address = $_POST["address"];
+
+        $editAdmin = new AdminContr();
+        $editAdmin->initEdit($id, $fullname, $email, $contact, $bday, $address);
     }
 ?>
