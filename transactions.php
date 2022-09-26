586a149c0871aa81483e9afb3dabc7c26a0c33ad
Justine <ujusine@yahoo.com>
2022-09-26 20:11:42 +0800

    updated layout not permitted users

--- transactions.php
+++ transactions.php
@@ -1,7 +1,7 @@
 <?php
 include "header.php";
 
-if ($_SESSION["is_superuser"] == 1) {
+if ($_SESSION["is_superuser"] == 1 || $_SESSION["is_cashier"]) {
 
 ?>
 
@@ -25,7 +25,6 @@ if ($_SESSION["is_superuser"] == 1) {
             </div>
             <h3>Transactions</h3>
         </div>
-        <a class="transaction-btn" href="transactions.php">Transactions</a>
     </nav>
 
     <div class="main-content tables-container">
@@ -40,9 +39,21 @@ if ($_SESSION["is_superuser"] == 1) {
 <?php } else {
 ?>
     <nav class="inventory-nav nav-page flex-row">
-        <div class="main-content admin-container">
-            <p>You do not have permission.</p>
+        <div class="title flex-row">
+            <div class="circle-svg">
+                <svg class="sidelink-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <path d="M0 96C0 60.65 28.65 32 64 32H448C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96zM64 160H128V96H64V160zM448 96H192V160H448V96zM64 288H128V224H64V288zM448 224H192V288H448V224zM64 416H128V352H64V416zM448 352H192V416H448V352z" />
+                </svg>
+            </div>
+            <h3>Transactions</h3>
         </div>
     </nav>
+
+    <div class="main-content tables-container">
+        <div class="tables flex-row">
+            <span>You do not have permission.</span>
+        </div>
+    </div>
 <?php }
 ?>
