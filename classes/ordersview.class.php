ec1524eb357c575d6bd70a10014b64cc4032a1bc
Justine <ujusine@yahoo.com>
2022-10-26 15:20:03 +0800

    fix table width

--- classes/ordersview.class.php
+++ classes/ordersview.class.php
@@ -5,26 +5,34 @@ class OrdersView extends Orders{
         $results = $this->getOrder($oid);
         ?>
 
-        <table class="tables-table order-table">
+        <table class="tables-table order-table" style="height: 300px">
             <thead>
-                <th>Crew</th>
+                <th style="width:15%">Crew</th>
                 <th>Order</th>
             </thead>
             <?php
-            foreach ($results as $row) {
             ?>
             <tbody>
                 <?php 
+                    foreach ($results as $row) {
                     if($row['is_ready'] == 1 ) {
-                    ?>
-                <tr>
-                    <td><?php echo $row["waiter"]; ?></td>
-                    <td><?php echo $row["order"] . " x " . $row["quantity"]; ?></td>
+?>
+                <tr style="overflow-y:hidden;">
+                    <td style="width:150px"><?php echo $row["waiter"]; ?></td>
+                    <?php
+                     
+                        ?>
+                    <td style="text-align: left;"><?php 
+                    $result2 = explode("|",$row['order']);
+                    $result3 = explode("|",$row['quantity']);
+                    for ($i=0; $i < (count($result2) - 1); $i++) {
+                    echo $result2[$i] . "(" . $result3[$i] . ")"; 
+                    }
+                    ?></td>
                 </tr>
-                
                 <?php
-                        }
                 }
+            }
                 ?>
             </tbody>
         </table>
