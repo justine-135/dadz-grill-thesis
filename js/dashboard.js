82b5d504aa847b14ac7e547fa54e6f88f8ecd67f
Justine <ujusine@yahoo.com>
2022-10-26 15:28:07 +0800

    output dashboard data

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,107 +5,185 @@ window.addEventListener("load", () => {
 
   let interval = 800;
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".sales-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 1, true);
     xmlhttp.send();
-  },interval)
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".order-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 2, true);
     xmlhttp.send();
-  },interval)
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".cancelled-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 3, true);
     xmlhttp.send();
-  },interval)
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".success-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 4, true);
     xmlhttp.send();
-  },interval);
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".occupied-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 5, true);
     xmlhttp.send();
-  },interval);
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".unoccupied-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 6, true);
     xmlhttp.send();
-  },interval);
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".call-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 7, true);
     xmlhttp.send();
-  },interval);
+  }, interval);
 
-  setInterval(()=>{
+  setInterval(() => {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
       if (this.readyState == 4) {
         const totalOrders = document.querySelector(".dirty-number");
-        
-        totalOrders.innerHTML = this.responseText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
     };
     xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 8, true);
     xmlhttp.send();
-  },interval);
+  }, interval);
+
+  setInterval(() => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        const totalOrders = document.querySelector(".total-crews-number");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
+    };
+    xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 9, true);
+    xmlhttp.send();
+  }, interval);
+
+  setInterval(() => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        const totalOrders = document.querySelector(
+          ".total-active-crews-number"
+        );
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
+    };
+    xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 10, true);
+    xmlhttp.send();
+  }, interval);
+
+  setInterval(() => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        const totalOrders = document.querySelector(".total-served-number");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
+    };
+    xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 11, true);
+    xmlhttp.send();
+  }, interval);
+
+  setInterval(() => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        const totalOrders = document.querySelector(".crew-role");
+
+        totalOrders.innerHTML = this.responseText
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      }
+    };
+    xmlhttp.open("GET", "./includes/dashboard-view.inc.php?grid=" + 12, true);
+    xmlhttp.send();
+  }, interval);
 });
