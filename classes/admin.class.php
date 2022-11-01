a4469803865d57427762cdb555e01da9fc0826ba
Justine <ujusine@yahoo.com>
2022-11-01 15:40:02 +0800

    add change name, username, contact, and password

--- classes/admin.class.php
+++ classes/admin.class.php
@@ -9,10 +9,77 @@ class Admin extends Dbh{
         header("location: ../admins.php?alert=deleted&id=" . $id);
     }
 
-    protected function editAdmin($id, $fullname, $email, $contact, $bday, $address){
-        $sql = "UPDATE users SET fullname = '$fullname', email = '$email', contact = $contact, birth_date = '$bday', location_address = '$address' WHERE id = $id";
+    protected function editName($id, $fname, $lname){
+        $fullName = $fname . " " . $lname;
+        $sql = "UPDATE users SET reg_date = reg_date, fname = '$fname', lname = '$lname', fullname = '$fullName' WHERE id = $id";
         $stmt = $this->connection()->prepare($sql);
         $stmt->execute();
+        $stmt = null;
+
+        $sql = "SELECT * FROM users WHERE id=$id";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll();
+
+        return $results;
+    }
+
+    protected function editUserName($id, $username){
+        $sql = "UPDATE users SET reg_date = reg_date, username = '$username' WHERE id = $id";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute();
+        $stmt = null;
+
+        $sql = "SELECT * FROM users WHERE id=$id";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll();
+
+        return $results;
+    }
+
+    protected function editPassword($id, $oldPwd, $newPwd, $reTypePwd){
+
+        $sql = "SELECT * FROM users WHERE id=?";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$id]);
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $hashedPass = substr($results[0]["pwd"], 0, 60);
+        $checkPass = password_verify($oldPwd, $hashedPass);
+
+        if ($checkPass === true) {
+            $newHashedPass = password_hash($newPwd, PASSWORD_DEFAULT);
+            $sql = "UPDATE users SET reg_date = reg_date, pwd = '$newHashedPass' WHERE id = ?";
+            $stmt = $this->connection()->prepare($sql);
+            $stmt->execute([$id]);
+            $stmt = null;
+
+            header("location: ../profile.php?alert" . $id);
+        }
+        else{
+            header("location: ../profile.php?message=invalid");
+
+        }
+
+    }
+
+    protected function editContact($id, $contact){
+        $sql = "UPDATE users SET reg_date = reg_date, contact = '$contact' WHERE id = $id";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute();
+        $stmt = null;
+
+        $sql = "SELECT * FROM users WHERE id=?";
+        $stmt = $this->connection()->prepare($sql);
+        $stmt->execute([$id]);
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $results;
     }
 
     protected function editRole($id, $role){
@@ -39,11 +106,10 @@ class Admin extends Dbh{
                 break;
             
             default:
-                # code...
                 break;
         }
 
     }
-}
 
+}
 ?>
