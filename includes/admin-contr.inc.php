541a6bab978c84998c84c1857e5ad060a6da7c2b
Justine <ujusine@yahoo.com>
2022-11-01 15:39:30 +0800

    removed old edit object

--- includes/admin-contr.inc.php
+++ includes/admin-contr.inc.php
@@ -8,15 +8,35 @@
         $deleteAdmin->initDelete($id);
     }
 
-    if (isset($_POST["edit"])) {
-        $fullname = $_POST["fullname"];
-        $email = $_POST["email"];
-        $contact = $_POST["contact"];
-        $bday = $_POST["bdate"];
-        $address = $_POST["address"];
+    if (isset($_POST["name-change"])) {
+        $fname = strtoupper($_POST['fname']);
+        $lname = strtoupper($_POST['lname']);
 
-        $editAdmin = new AdminContr();
-        $editAdmin->initEdit($id, $fullname, $email, $contact, $bday, $address);
+        $editName = new AdminContr();
+        $editName->initEditName($id, $fname, $lname);
+    }
+
+    if (isset($_POST["username-change"])) {
+        $username = $_POST["username"];
+
+        $editUserName = new AdminContr();
+        $editUserName->initEditUserName($id, $username);
+    }
+
+    if (isset($_POST["contact-change"])) {
+        $contact = $_POST["contact"];
+
+        $editContact = new AdminContr();
+        $editContact->initEditContact($id, $contact);
+    }
+
+    if (isset($_POST["pwd-change"])) {
+        $oldPwd = $_POST["old-pwd"];
+        $newPwd = $_POST["new-pwd"];
+        $reTypePwd = $_POST["retype-pwd"];
+
+        $editPassword = new AdminContr();
+        $editPassword->initEditPassword($id, $oldPwd, $newPwd, $reTypePwd);
     }
 
     if (isset($_POST['edit-role'])) {
