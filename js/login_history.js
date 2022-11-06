db93bfe6eb285c40c28615eb20d4d62d95a43181
Justine <ujusine@yahoo.com>
2022-11-06 12:00:37 +0800

    export data

--- js/login_history.js
+++ js/login_history.js
@@ -10,4 +10,34 @@ window.addEventListener("load", () => {
     xmlhttp.open("GET", "./includes/loginhistory-view.inc.php", true);
     xmlhttp.send();
   }, 1000);
+
+  const exportBtn = document.querySelector(".csvHtml5");
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const d = new Date();
+  let month = months[d.getMonth()];
+  let day = d.getDate();
+
+  let fileName = month + " " + day + " Login History";
+
+  exportBtn.addEventListener("click", () => {
+    var table2excel = new Table2Excel({
+      defaultFileName: fileName,
+    });
+    table2excel.export(document.querySelectorAll(".loginhistory-table-info"));
+  });
 });
