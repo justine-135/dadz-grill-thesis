db596cf13dc750d68401f505b9fbbf8bd78b57d6
Justine <ujusine@yahoo.com>
2022-11-08 14:05:54 +0800

    access view of login history

--- includes/loginhistory-view.inc.php
+++ includes/loginhistory-view.inc.php
@@ -1,6 +1,18 @@
 <?php
     include 'autoload.inc.php';
     
-    $allUser = new UsersView();
-    $allUser->initGetHistory();
+    if (isset($_GET["view"])) {
+        $view = $_GET["view"];
+
+        if ($view == 1) {
+            $loginHistory = new UsersView();
+            $loginHistory->initGetHistory();
+        }
+
+        if ($view == 2) {
+            $date = $_GET["date"];
+            $loginHistory = new UsersView();
+            $loginHistory->initGetHistoryDate($date);
+        }
+    }
 ?>
