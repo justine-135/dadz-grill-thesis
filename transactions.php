e9d7def86e2b28e891fed35f143bd0de15eb265e
Justine <ujusine@yahoo.com>
2022-11-10 13:03:56 +0800

    updated

--- transactions.php
+++ transactions.php
@@ -23,27 +23,40 @@ if ($_SESSION["is_superuser"] == 1 || $_SESSION["is_cashier"]) {
                     <path d="M0 96C0 60.65 28.65 32 64 32H448C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96zM64 160H128V96H64V160zM448 96H192V160H448V96zM64 288H128V224H64V288zM448 224H192V288H448V224zM64 416H128V352H64V416zM448 352H192V416H448V352z" />
                 </svg>
             </div>
-            <h3>Transactions</h3>
+            <h4>Transactions</h4>
         </div>
-        <input type="date" name="" id="" class="search-transaction">
-        <select id="transaction-export" class="select">
+        <div class="dropdown date-transaction ">
+            <button class="btn dropdown-toggle border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Select date
+            </button>
+            <ul class="dropdown-menu">
+                <li class="ps-2">
+                    <span>Start time:</span>
+                    <input type="date" name="" id="" class="search-transaction" placeholder="start date">
+                </li>
+                <li class="ps-2 mt-2">
+                    <span>End time:</span>
+                    <input type="date" name="" id="" class="search-transaction-2" placeholder="start date">
+                </li>
+            </ul>
+        </div>
+        <select class="form-select form-select select-date ms-1" id="transaction-export" aria-label=".form-select-sm example">
             <option value="export-transaction-opt">Transaction</option>
             <option value="export-sales-report-opt">Sales Report</option>
         </select>
-        <button class="add-item-btn csvHtml5-transaction">Export</button>
+        <button class="btn bg-light ms-1 csvHtml5-transaction">Export</button>
     </nav>
 
     <div class="main-content tables-container">
         <div class="tables flex-row">
             <table class="tables-table transaction-tbl-data hide"></table>
-            <table class="tables-table transaction-tbl-data-date"></table>
+            <table class="tables-table transaction-tbl-data-date table"></table>
             <table class="export-transaction-tbl hide"></table>
             <table class="export-transaction-tbl-date hide"></table>
             <table class="export-sales-report-tbl hide"></table>
             <table class="export-sales-report-tbl-date hide"></table>
         </div>
     </div>
-
     <script src="js/transactions.js"></script>
 
 <?php } else {
