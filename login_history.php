a1a9e739916e216696a9ff44816c27af1c6df7a4
Justine <ujusine@yahoo.com>
2022-11-10 13:03:04 +0800

    updated

--- login_history.php
+++ login_history.php
@@ -11,18 +11,33 @@ if ($_SESSION["is_superuser"] == 1) {
                     <path fill="currentColor" d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"></path>
                 </svg>
             </div>
-            <h3>History</h3>
+            <h4>History</h4>
         </div>
-        <input type="date" name="" id="" class="search-login-history">
+        <!-- <input type="date" name="" id="" class="search-login-history"> -->
+        <div class="dropdown date-transaction ">
+            <button class="btn dropdown-toggle border" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Select date
+            </button>
+            <ul class="dropdown-menu">
+                <li class="ps-2">
+                    <span>Start time:</span>
+                    <input type="date" name="" id="" class="search-login-history" placeholder="start date">
+                </li>
+                <li class="ps-2 mt-2">
+                    <span>End time:</span>
+                    <input type="date" name="" id="" class="search-login-history-2" placeholder="start date">
+                </li>
+            </ul>
+        </div>
         <button class="add-item-btn csvHtml5">Export</button>
 
     </nav>
 
     <div class="main-content tables-container">
         <div class="tables">
-            <table class="tables-table loginhistory-table-info hide">
+            <table class="tables-table loginhistory-table-info table hide">
             </table>
-            <table class="tables-table loginhistory-table-info-date">
+            <table class="tables-table loginhistory-table-info-date table">
             </table>
         </div>
     </div>
