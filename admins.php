f2ea92c81f9af4fa0daabbab12860a379499403e
Justine <ujusine@yahoo.com>
2022-11-10 13:02:36 +0800

    updated

--- admins.php
+++ admins.php
@@ -45,7 +45,7 @@ if ($_SESSION["is_superuser"] == 1) {
                     <path fill="currentColor" d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"></path>
                 </svg>
             </div>
-            <h3>Accounts</h3>
+            <h4>Accounts</h4>
         </div>
         <a href="login_history.php" class="add-admin-btn">History</a>
         <a href="registration.php?message" class="add-admin-btn">Add crew</a>
@@ -53,7 +53,7 @@ if ($_SESSION["is_superuser"] == 1) {
 
     <div class="main-content tables-container">
         <div class="tables">
-            <table class="tables-table admin-table-info">
+            <table class="tables-table admin-table-info table">
             </table>
         </div>
     </div>
