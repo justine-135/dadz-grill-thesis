7ab3f3686586eedac589242f7c578eae0ed35d12
Justine <ujusine@yahoo.com>
2022-11-15 14:30:02 +0800

    change heading from h4 to h5

--- profile.php
+++ profile.php
@@ -14,10 +14,10 @@
 <div class="main-content tables-container">
     <div class="tables flex-row edit-profile-div">
         <div class="body flex-column edit-profileinfo-div">
-            <ul>
+            <ul style="margin-left: -20px !important">
                 <li class="">
                     <div class="profile-li flex-row">
-                        <h4>Name</h4>
+                        <h5>Name</h5>
                         <span><?php echo $_SESSION["fullname"] ?></span>
                         <button class="edit-show flex-row">
                             <svg xmlns="http://www.w3.org/2000/svg" class="sidelink-svg-sm" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
@@ -42,7 +42,7 @@
                 </li>
                 <li class="">
                     <div class="profile-li flex-row">
-                        <h4>Username</h4>
+                        <h5>Username</h5>
                         <span><?php echo $_SESSION["username"] ?></span>
                         <button class="edit-show flex-row">
                             <svg xmlns="http://www.w3.org/2000/svg" class="sidelink-svg-sm" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
@@ -63,7 +63,7 @@
                 </li>
                 <li class="">
                     <div class="profile-li flex-row">
-                        <h4>Contact</h4>
+                        <h5>Contact</h5>
                         <span><?php echo $_SESSION["contact"] ?></span>
                         <button class="edit-show flex-row">
                             <svg xmlns="http://www.w3.org/2000/svg" class="sidelink-svg-sm" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
@@ -84,7 +84,7 @@
                 </li>
                 <li class="">
                     <div class="profile-li flex-row">
-                        <h4>Password</h4>
+                        <h5>Password</h5>
                         <span>Set password</span>
                         <button class="edit-show flex-row">
                             <svg xmlns="http://www.w3.org/2000/svg" class="sidelink-svg-sm" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
