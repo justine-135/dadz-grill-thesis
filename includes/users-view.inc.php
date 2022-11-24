eb7b39c67e97c6656091c9359e63133151c77001
Justine <ujusine@yahoo.com>
2022-11-24 13:41:49 +0800

    updated includes

--- includes/users-view.inc.php
+++ includes/users-view.inc.php
@@ -1,6 +1,17 @@
 <?php
     include 'autoload.inc.php';
     
-    $allUser = new UsersView();
-    $allUser->initGetUsers();
+    if (isset($_GET["view"])) {
+        $view = $_GET["view"];
+        if ($view == 1) {
+            $allUser = new UsersView();
+            $allUser->initGetUsers();
+        }
+        if ($view == 2) {
+            $date = $_GET["date"];
+            $date2 = $_GET["date2"];
+            $userCompliance = new UsersView();
+            $userCompliance->initUserCompliance($date, $date2);
+        }
+    }
 ?>
