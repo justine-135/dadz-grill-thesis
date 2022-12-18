05b6d45a5b8a24fb33d92959590fdc4aa150d35f
Justine <ujusine@yahoo.com>
2022-12-18 16:04:05 +0800

    Changed title and added icon

--- login.php
+++ login.php
@@ -11,11 +11,12 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/icons/logo.jpg" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./styles/styles.css">
-    <title>Login</title>
+    <title>Dadz Grillhouse</title>
 </head>
 <body>
     <form class="login-form" action="./includes/user-contr.inc.php" method="post" enctype="multipart/form-data">
